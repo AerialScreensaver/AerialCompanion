797e18699586586f82e9a0461a45a656c77f123c
Guillaume Louel <guillaume@louel.fr>
2020-07-24 14:33:47 +0200

    Initial Commit

---
+++ AerialUpdater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AerialUpdater
+//
+//  Created by Guillaume Louel on 24/07/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AerialUpdater/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AerialUpdater
+//
+//  Created by Guillaume Louel on 24/07/2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
